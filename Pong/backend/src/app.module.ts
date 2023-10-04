bb6cb98372876aa7bce26ee009cdfd92b21891a3
Wonderworlds <fmauguin.pro@gmail.com>
2023-10-04 21:46:03 +0200

    gestion de .env backend <=> db

--- Pong/backend/src/app.module.ts
+++ Pong/backend/src/app.module.ts
@@ -6,17 +6,24 @@ import { User } from './typeorm/entities/User';
 import { UsersModule } from './users/users.module';
 import { Profile } from './typeorm/entities/Profile';
 import { Post } from './typeorm/entities/Post';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-	type: 'postgres',
-	host: 'postgres',
-	port: 5432,
-	username: 'postgres',
-	password: 'mdp',
-	database: 'pong_db',
-	entities: [User, Profile, Post],
-	synchronize: true
+  imports: [
+	ConfigModule.forRoot({ isGlobal: true}),
+	TypeOrmModule.forRootAsync({
+		imports: [ConfigModule],
+		useFactory: (configService: ConfigService) => ({
+			type: 'postgres',
+			host: configService.get('POSTGRES_HOSTNAME'),
+			port: 5432,
+			username: configService.get('POSTGRES_USER'),
+			password: configService.get('POSTGRES_PASSWORD'),
+			database: configService.get('POSTGRES_DB'),
+			entities: [User, Profile, Post],
+			synchronize: true
+		}),
+			inject: [ConfigService],
   }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
