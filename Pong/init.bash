30955ee15cb98eed3546adc11177349723156d97
Wonderworlds <fmauguin.pro@gmail.com>
2023-10-05 01:35:17 +0200

    works directly from gitclone

--- Pong/init.bash
+++ Pong/init.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-	/bin/bash -ec 'yarn --cwd backend/ run start:dev &'
-	sleep 5
-	/bin/bash -ec 'yarn --cwd frontend/ dev'
+	/bin/bash -ec 'yarn --cwd backend && yarn --cwd backend run start:dev &'
+	sleep 3
+	/bin/bash -ec 'yarn --cwd frontend && yarn --cwd frontend dev'
