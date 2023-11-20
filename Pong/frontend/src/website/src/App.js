b67283cd6fc2803f700bdbda265f3745b10a43e4
bifrah_home <benj.ifrah@gmail.com>
2023-11-20 14:47:54 +0100

    Redirection + css for Parameters

--- Pong/frontend/src/website/src/App.js
+++ Pong/frontend/src/website/src/App.js
@@ -5,21 +5,31 @@ import MainPage from './pages/MainPage';
 import Home from './pages/Home';
 import WaitingMatch from './pages/WaitingMatch';
 import Profile from './pages/Profile';
+import MatchHistory from './pages/MatchHistory';
+import Achievement from './pages/Achievement';
+import Friends from './pages/Friends';
+import Leaderboard from './pages/Leaderboard';
+import Parameters from './pages/Parameters';
 
-  
-  const App = () => {
-    return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/Home" element={<Home/>} />
-          <Route path="/WaitingMatch" element={<WaitingMatch/>} />
-          <Route path="/Profile" element={<Profile/>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-    );
+	
+	const App = () => {
+		return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<MainPage />} />
+					<Route path="/Home" element={<Home />} />
+					<Route path="/WaitingMatch" element={<WaitingMatch />} />
+					<Route path="/Profile" element={<Profile />} />
+						<Route path="/Profile/MatchHistory" element={<MatchHistory />} />
+						<Route path="/Profile/Achievement" element={<Achievement />} />
+						<Route path="/Profile/Friends" element={<Friends />} />
+						<Route path="/Profile/Leaderboard" element={<Leaderboard />} />
+					<Route path="/Parameters" element={<Parameters />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+		);
 };
 
 export default App;
