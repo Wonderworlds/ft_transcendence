8969b4730c145ac303de3ee82a827d97f2b8d814
bifrah_home <benj.ifrah@gmail.com>
2023-12-26 19:22:58 +0100

    add reusable dropdown button

---
+++ Pong/frontend/src/website/src/components/DropdownMenu.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+interface DropdownMenuProps {
+	dropdownButtonName: string;
+	dropdownActions: {
+		name: string;
+		onClick: () => void;
+	}[];
+}
+
+const DropdownMenu: React.FC<DropdownMenuProps> = ({dropdownButtonName, dropdownActions}) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+    <div className="headerDropdownMenu">
+		<button className="dropdownMenu" onClick={() => setIsOpen(prevState => !prevState)}>{dropdownButtonName}</button>
+		{isOpen && 
+		<div className="dropdownContent">
+			<ul>{dropdownActions.map(dropdownAction => <li key={dropdownAction.name}><button onClick={dropdownAction.onClick}>{dropdownAction.name}</button></li>)}</ul>
+		</div>
+		}
+    </div>
+  );
+};
+
+export default DropdownMenu;
