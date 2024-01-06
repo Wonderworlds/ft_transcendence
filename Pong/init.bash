ed9bbaa216beb987d30379f77e4469f22c9b79f9
Wonderworlds <fmauguin.pro@gmail.com>
2024-01-06 23:48:58 +0100

    init bash without workspaces

--- Pong/init.bash
+++ Pong/init.bash
@@ -2,6 +2,5 @@
 	/bin/bash -ec 'mkdir backend/secrets'
 	/bin/bash -ec 'openssl genpkey -algorithm RSA -out backend/secrets/private-key.pem'
 	/bin/bash -ec 'openssl req -new -key backend/secrets/private-key.pem -x509 -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=Transcendence/UID=Transcendence" -out backend/secrets/public-certificate.pem'
-	/bin/bash -ec 'yarn install'
-	/bin/bash -ec 'yarn --cwd backend run start:dev &'
-	/bin/bash -ec 'yarn --cwd frontend dev'
+	/bin/bash -ec 'yarn --cwd backend && yarn --cwd backend run start:dev &'
+	/bin/bash -ec 'yarn --cwd frontend && yarn --cwd frontend dev'
